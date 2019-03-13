dec9a0185177580fe2bfe72e670f24a2b4e440b7
Rainyforest <rainyforest4869@gmail.com>
2019-03-13 14:11:10 -0700

    Add files via upload

---
+++ A1/Cube2.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube2 : MonoBehaviour
+{
+    public Texture2D RayTracingResult;
+    public Texture2D pattern;
+    public static Color BackgroundColor = Color.grey;
+
+    public static Vector3 RayOrigin = Vector3.zero; 
+    public static Vector3 VPCentre = Vector3.forward;
+    
+    private float cal33Det(float[] arr){
+        // 0 1 2 
+        // 3 4 5
+        // 6 7 8 
+      
+        return arr[0]*(arr[4]*arr[8]-arr[5]*arr[7])-arr[1]*(arr[3]*arr[8]-arr[5]*arr[6])+arr[2]*(arr[3]*arr[7]-arr[4]*arr[6]);
+    }
+    private Color IntersectTriangle(Vector3 origin,Vector3 direction,Vector3 vA, Vector3 vB,Vector3 vC){   
+
+        Vector3 N = Vector3.Cross((vA - vB), (vA - vC));
+        float t = Vector3.Dot(N, (vA - origin))/(float)Vector3.Dot(N, direction);
+        Vector3 p = origin + direction * t;
+        float alpha,beta,gamma;
+        float ax= vA.x;
+        float ay= vA.y;
+        float az= vA.z;
+
+        float bx= vB.x;
+        float by= vB.y;
+        float bz= vB.z;
+
+        float cx= vC.x;
+        float cy= vC.y;
+        float cz= vC.z;
+
+        float dx= direction.x;
+        float dy= direction.y;
+        float dz= direction.z;
+        float[] det_beta =  new float[9]  {ax, ax-cx,dx,
+                                    ay, ay-cy,dy,
+                                    az, az-cz,dz};
+        float[] det_gamma = new float[9] {ax - bx, ax,dx,
+                                    ay - by, ay,dy,
+                                    az - bz, az,dz};
+        float[] det_t = new float[9] {ax - bx, ax - cx,ax,
+                                ay - by, ay - cy,ay,
+                                az - bz, az - cz,az};
+        float[] det_A = new float[9] {ax - bx, ax - cx,dx,
+                                ay - by, ay - cy,dy,
+                                az - bz, az - cz,dz};
+        float det_A_val = cal33Det(det_A); 
+        beta = cal33Det(det_beta)/det_A_val;
+        gamma = cal33Det(det_gamma)/det_A_val;
+        t = cal33Det(det_t)/det_A_val;
+        alpha = 1 - beta - gamma;
+        return ( alpha <= 0 || alpha >=1 || beta <= 0 || beta >=1 || gamma <= 0 || gamma >=1)?BackgroundColor : new Color(alpha,beta,gamma);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Camera this_camera = gameObject.GetComponent<Camera>();
+        Debug.Assert(this_camera);
+        int pixel_width = this_camera.pixelWidth;
+        int pixel_height = this_camera.pixelHeight;
+
+        RayTracingResult = new Texture2D(pixel_width, pixel_height);
+        /////
+        float ViewportWidth = 3;
+        float ViewportHeight = ViewportWidth  / pixel_width * pixel_height;
+
+        float VPWidthHalf = ViewportWidth / 2;
+        float VPHeightHalf = ViewportHeight / 2;
+
+        int pixel_width_half = pixel_width / 2;
+        int pixel_height_half = pixel_height / 2;
+
+        List<Vector3> vertices = new List<Vector3>();
+        vertices.Add(new Vector3(-4, -2.8f, 10)); //0
+        vertices.Add(new Vector3(-4, 2.8f, 10));  //1
+        vertices.Add(new Vector3(0, -2.8f, 9));   //2
+        vertices.Add(new Vector3(0, 2.8f, 9));    //3
+        vertices.Add(new Vector3(4, -2.8f, 10));  //4
+        vertices.Add(new Vector3(4, 2.8f, 10));   //5
+//new Vector3(-4, -2.8f, 10),new Vector3(-4, 2.8f, 10),new Vector3(0, -2.8f, 9)
+        for(int i = 0;i < pixel_width;++i){
+            for(int j = 0; j < pixel_height;++j){
+                Vector3 RayDirection = VPCentre;
+                RayDirection.x = (i - pixel_width_half) / (float)pixel_width_half * VPWidthHalf;
+                RayDirection.y = (j - pixel_height_half) / (float)pixel_height_half * VPHeightHalf;
+                Color pixelColor = BackgroundColor;
+                Vector3 Direction = RayDirection.normalized;
+                // 1   3   5
+                //
+                // 0   2   4
+                Color Tri_1 = IntersectTriangle(RayOrigin,Direction,vertices[0],vertices[1],vertices[2]); // (0,0) (0,1) (1,0)
+                Color Tri_2 = IntersectTriangle(RayOrigin,Direction,vertices[2],vertices[1],vertices[3]); // (1,0) (0,1) (1,1)
+                Color Tri_3 = IntersectTriangle(RayOrigin,Direction,vertices[2],vertices[3],vertices[5]); // (1,0) (1,1) (0,1)
+                Color Tri_4 = IntersectTriangle(RayOrigin,Direction,vertices[2],vertices[5],vertices[4]); // (1,0) (0,1) (0,0)
+                Vector2 TextureCoordinate = new Vector2(0,0);
+                float pattern_width = pattern.width;
+                float pattern_height = pattern.height;
+
+                if(Tri_1!=BackgroundColor||Tri_2!=BackgroundColor||Tri_3!=BackgroundColor||Tri_4!=BackgroundColor){
+                    if(Tri_1!=BackgroundColor){
+                        TextureCoordinate = Tri_1.r*new Vector2(0,0) + Tri_1.g*new Vector2(0,1) + Tri_1.b*new Vector2(1,0);
+                    } else if(Tri_2!=BackgroundColor){
+                        TextureCoordinate = Tri_2.r*new Vector2(1,0) + Tri_2.g*new Vector2(0,1) + Tri_2.b*new Vector2(1,1);
+                    } else if(Tri_3!=BackgroundColor){
+                        TextureCoordinate = Tri_3.r*new Vector2(1,0) + Tri_3.g*new Vector2(1,1) + Tri_3.b*new Vector2(0,1);
+                    } else if(Tri_4!=BackgroundColor){
+                        TextureCoordinate = Tri_4.r*new Vector2(1,0) + Tri_4.g*new Vector2(0,1) + Tri_4.b*new Vector2(0,0);
+                    } else{}
+                    pixelColor = pattern.GetPixel((int)(TextureCoordinate.x*pattern_width),(int)(TextureCoordinate.y*pattern_height));
+                }
+               
+                RayTracingResult.SetPixel(i,j,pixelColor);
+            }
+        }
+        RayTracingResult.Apply();
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //Show the generated ray tracing image on screen
+        Graphics.Blit(RayTracingResult, destination);
+    }
+}
---
+++ A1/Sphere.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Sphere : MonoBehaviour {
+    public Texture2D RayTracingResult;
+
+    public static Color BackgroundColor = Color.grey;
+    public static Color AmbientColor = Color.yellow; //R G B defined by myself
+    public static Color LightColor = Color.white;
+                              
+    public static Vector3 LightDirection = new Vector3(1,2,3);
+
+    public static Vector3 RayOrigin = Vector3.zero; 
+    public static Vector3 VPCentre = Vector3.forward;
+    public static Vector3 SphereCentre = new Vector3(5,6,10); //Define the position of spherecentre
+    public static float SphereRadius = 1;
+
+    float ifIntersect(Vector3 RayDirection){
+        //Determine the ray intersecting with the sphere or not
+       
+        Vector3 Direction = RayDirection.normalized;
+        // Vector3 intersect_normal;      
+        Vector3 OE = SphereCentre - RayOrigin;
+        float v = Vector3.Dot(OE,Direction);
+        float RadiusSquare = SphereRadius*SphereRadius;
+        float OE_Square = Vector3.Dot(OE,OE);
+        return RadiusSquare - (OE_Square - v*v);    //return the discriminant of intersection formula
+    }
+       
+    Color renderPixel(Color pixelColor, Vector3 RayDirection,float discriminant){
+        //Determine the ray intersecting with the sphere or not
+        Vector3 Direction = RayDirection.normalized;
+        // Vector3 intersect_normal;
+        float t;    
+        Vector3 OE = SphereCentre - RayOrigin;
+        float v = Vector3.Dot(OE,Direction);
+        float diffuseStrength = 0.3f;
+        float specularStrength = 0.5f;
+        float specularPower = 4;
+        float d = Mathf.Sqrt(discriminant);
+        t = v - d;
+        Vector3 Intersection = RayOrigin + Direction * t;   
+        Vector3 normal = RayOrigin + Direction * v - SphereCentre;
+        //calculation of the shaders
+        pixelColor = AmbientColor;
+        float diffuse = Vector3.Dot(normal,LightDirection) *diffuseStrength;
+        pixelColor += LightColor * diffuse;
+        Vector3 view = RayDirection*(-1);
+        Vector3 half = view + RayDirection;
+        // specular
+        float blinn = Vector3.Dot(half,normal);
+        float specular = Mathf.Pow(blinn, specularPower)*specularStrength;
+        pixelColor += LightColor * specular;
+        return pixelColor;
+    }
+    void Start () {
+        /////
+        Camera this_camera = gameObject.GetComponent<Camera>();
+        Debug.Assert(this_camera);
+        int pixel_width = this_camera.pixelWidth;
+        int pixel_height = this_camera.pixelHeight;
+
+        RayTracingResult = new Texture2D(pixel_width, pixel_height);
+        /////
+        float ViewportWidth = 3;
+        float ViewportHeight = ViewportWidth  / pixel_width * pixel_height;
+
+        float VPWidthHalf = ViewportWidth / 2;
+        float VPHeightHalf = ViewportHeight / 2;
+
+        int pixel_width_half = pixel_width / 2;
+        int pixel_height_half = pixel_height / 2;
+
+        for(int i = 0;i < pixel_width;++i){
+            for(int j = 0; j < pixel_height;++j){
+                Vector3 RayDirection = VPCentre;
+                RayDirection.x = (i - pixel_width_half) / (float)pixel_width_half * VPWidthHalf;
+                RayDirection.y = (j - pixel_height_half) / (float)pixel_height_half * VPHeightHalf;
+                Color pixelColor = Color.yellow;
+
+                float discriminant = ifIntersect(RayDirection);
+                RayTracingResult.SetPixel(i,j,discriminant>0?renderPixel(pixelColor,RayDirection,discriminant):BackgroundColor); 
+            }
+        }
+        RayTracingResult.Apply();
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //Show the generated ray tracing image on screen
+        Graphics.Blit(RayTracingResult, destination);
+    }
+}
